0ad47a9b8ed00ed8bc5d5f9cb52411a9a85dbfb9
Nikita <avdeevn1142@gmail.com>
2024-04-05 09:17:38 +0300

    Create function compare_files which prepare files to find same and diff for methods find_same_lines and find_diff_lines and them write on the same.txt and diff.txt

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+def find_same_lines(set1, set2):
+    pass
+
+
+def find_diff_lines(set1, set2):
+    pass
+
+
+def compare_files(file1, file2):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        set1 = set(line.strip() for line in f1)
+        set2 = set(line.strip() for line in f2)
+
+    same = find_same_lines(set1, set2)
+    diff = find_diff_lines(set1, set2)
+
+    with open('same.txt', 'w') as f:
+        for line in same:
+            f.write(line + '\\n')
+
+    with open('diff.txt', 'w') as f:
+        for line in diff:
+            f.write(line + '\\n')
+
+
+
+
+
 if __name__ == '__main__':
     pass
